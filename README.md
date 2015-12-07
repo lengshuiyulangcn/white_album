92d4e873c6c9f8fcaf44325d5b04557b973283a2
lengshuiyulangcn <li.ning@i.mbox.nagoya-u.ac.jp>
2015-12-07 23:01:20 +0900

    base

---
+++ README.md
@@ -0,0 +1,6 @@
+### White album
+
+From one of my favourate Game title.
+
+It will be a rails album application.
+
---
+++ app/models/photo.rb
@@ -0,0 +1,12 @@
+class Photo < ActiveRecord::Base
+  validates :title, presence: true
+  mount_uploader :image, ImageUploader
+  belongs_to :album
+
+  def self.parse_filename(filename)
+    filename.gsub!(/(.jpg|.png)/, '')
+    return nil unless filename =~ /^\w*-(([a-zA-Z])*(_|$))*/
+    filename.split('_').join(' ')
+    {title: filename}
+  end
+end
